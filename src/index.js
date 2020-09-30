c94f21d3e3fd1886ab78056d9986faa5002fb3ee
Artem <temaspsxxx@gmail.com>
2020-09-30 11:56:42 +0300

    feat: add redux store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { createStore } from 'redux'
 
 const reducer = (state = 0, action) => {
   switch (action.type) {
@@ -6,10 +7,9 @@ const reducer = (state = 0, action) => {
   }
 };
 
-let state = reducer(undefined, {});
-
-state = reducer(state, { type: 'INC' });
-console.log(state);
-
-state = reducer(state, { type: 'INC' });
-console.log(state);
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log(store.getState())
+})
+store.dispatch({ type: 'INC' })
+store.dispatch({ type: 'INC' })
